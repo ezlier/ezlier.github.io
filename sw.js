776b944bdd16b55bc61422a5a213464c9dedf130
ezlier <86654833@qq.com>
2024-10-05 12:13:43 +0800

    Site updated: 2024-10-05 12:13:43

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1728101468280";
+const VERSION = "1728101608735";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
